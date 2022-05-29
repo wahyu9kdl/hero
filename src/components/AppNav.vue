e1c264afa300dcbcc31392a7a7575e44cfd684ba
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-05-29 11:12:13 +0700

    Create AppNav.vue

---
+++ src/components/AppNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    Made by
+    <a href="https://wahyu9kdl.github.io/">awdev</a>, deployed on
+    
+    <app-github-corner />
+  </nav>
+</template>
+
+<script>
+import AppGithubCorner from "./AppGithubCorner.vue";
+
+export default {
+  components: {
+    AppGithubCorner
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: white;
+  padding: 25px 40px;
+  font-size: 18px;
+  font-family: "Whitney A", "Whitney B";
+  font-style: italic;
+  font-weight: 600;
+}
+
+@media screen and (max-width: 600px) {
+  nav {
+    padding: 15px 20px;
+  }
+}
+</style>
