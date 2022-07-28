661e901fa13c0ed85370617842fa65368c48b842
AWDEV CORPORATION <84139744+wahyu9kdl@users.noreply.github.com>
2022-07-28 13:01:33 +0700

    Add files via upload

---
+++ github-explorer/precache-manifest.5f5640b6899b1f74ecda48d75febcc40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561d8a0849c73baf96e992910cdf10fe",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "94ef3d66bc0ced5fad76",
+    "url": "/github-explorer/static/js/2.775c0e9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.775c0e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4218c882ab99af29f248",
+    "url": "/github-explorer/static/js/main.3bd74bfa.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
